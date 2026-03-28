7bc51c1964ed53f21f9f19ceb197e682e80539c1
Harsh99960 <168182041+Harsh99960@users.noreply.github.com>
2026-03-28 16:32:17 +0530

    Time: 144 ms (7.32%), Space: 28 MB (98.81%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        //insertion sort 
+        for(int i = 1; i<n; i++){
+            int j = i;
+            while(j>=1){
+                if(nums[j]>=nums[j-1]) break;
+                else swap(nums[j],nums[j-1]);
+                j--;
+            }
+        }
+
+        //counting logic can be used here as per my choice
+
+        int count = 1;
+        int ans = nums[0];
+        for(int i =1 ; i<n; i++){
+            if(nums[i]==nums[i-1]){
+                count ++;
+            } else{
+                count =1;
+            }
+
+            if(count>n/2){
+                ans = nums[i];
+                break;
+            }
+        }
+
+        return ans;
+    } 
+};
