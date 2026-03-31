c07df909ae70662941055482a6b031425c731633
Harsh99960 <168182041+Harsh99960@users.noreply.github.com>
2026-03-31 19:01:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,18 +2,38 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-
-        int actualSum = n * (n+1)/2 ;
-
-        int realSum = 0;
-        for(int i =0 ; i<n ; i++){
-            realSum += nums[i];
+        int i = 0;
+        while(i<n){
+            int correctidx = nums[i];
+            if(correctidx == i || nums[i]==n) i++;
+            else swap(nums[i], nums[correctidx]);
         }
 
-        return actualSum - realSum;
+        for(int i = 0; i<n;i++){
+            if(nums[i]!=i) return i;
+        }
+
+        return n ;
     }
 };
 
+
+
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n = nums.size();
+
+//         int actualSum = n * (n+1)/2 ;
+
+//         int realSum = 0;
+//         for(int i =0 ; i<n ; i++){
+//             realSum += nums[i];
+//         }
+
+//         return actualSum - realSum;
+//     }
+// };
 
 
 // class Solution {
