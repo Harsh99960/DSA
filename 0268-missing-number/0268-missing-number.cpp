2389cbf4b4082e8c077df8771a3f800b35d56b0a
Harsh99960 <168182041+Harsh99960@users.noreply.github.com>
2026-03-31 18:32:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,17 +2,35 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        vector<bool> check(n+1 , false);
-        for(int i =0;i<n;i++){
-             int ele = nums[i];
-            check[ele]=true;
+
+        int actualSum = n * (n+1)/2 ;
+
+        int realSum = 0;
+        for(int i =0 ; i<n ; i++){
+            realSum += nums[i];
         }
 
-
-        for(int i = 0; i<=n ; i++){
-            if(check[i]==false) return i;
-        }
-
-        return 0;
+        return actualSum - realSum;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<bool> check(n+1 , false);
+//         for(int i =0;i<n;i++){
+//              int ele = nums[i];
+//             check[ele]=true;
+//         }
+
+
+//         for(int i = 0; i<=n ; i++){
+//             if(check[i]==false) return i;
+//         }
+
+//         return 0;
+//     }
+// };
