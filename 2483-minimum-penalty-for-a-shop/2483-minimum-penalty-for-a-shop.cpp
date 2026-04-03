233c8c376f19322c6c47341c8ec583fa2c6980f1
Harsh99960 <168182041+Harsh99960@users.noreply.github.com>
2026-04-03 21:42:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int bestClosingTime(string customers) {
         int n = customers.size();
-
-        // STEP 1: total Y count karo
         int totalY = 0;
         for(char c : customers){
             if(c == 'Y') totalY++;
@@ -15,22 +13,14 @@ public:
         int minPenalty = INT_MAX;
         int answer = 0;
 
-        // STEP 2: har closing time try karo
+         
         for(int i = 0; i <= n; i++){
-
-            // STEP 3: right side Y nikaalo
             int rightY = totalY - leftY;
-
-            // STEP 4: penalty calculate
             int penalty = leftN + rightY;
-
-            // STEP 5: minimum track karo
             if(penalty < minPenalty){
                 minPenalty = penalty;
                 answer = i;
             }
-
-            // STEP 6: next iteration ke liye update
             if(i < n){
                 if(customers[i] == 'Y'){
                     leftY++;
